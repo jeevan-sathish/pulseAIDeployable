a2cd85300f56e44b314df09dc6316650489bf2d6
Jeevan s <sathishjeevan2004@gmail.com>
2026-05-04 12:37:45 +0530

    render backend api added

--- frontend/src/components/Prediction.jsx
+++ frontend/src/components/Prediction.jsx
@@ -70,7 +70,7 @@ const Prediction = () => {
     setLoading(true);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/predict", {
+      const res = await fetch("https://pulseai-kmyk.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
